994ebb35cde91dcc68f4ce509c3e4cec63916964
francesca2 <francesca.giordani88@gmail.com>
2016-11-18 10:24:48 +0100

    metodi per prendere i parametri

--- SecondoEsempioRest/src/main/java/it/alfasoft/francesca/resources/TestApi.java
+++ SecondoEsempioRest/src/main/java/it/alfasoft/francesca/resources/TestApi.java
@@ -1,10 +1,18 @@
 package it.alfasoft.francesca.resources;
 
+import it.alfasoft.francesca.bean.FiltriFattura;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("test")
 @Produces(MediaType.TEXT_PLAIN)
@@ -21,4 +29,31 @@ public class TestApi {
 	public String getHeaderParameter(@HeaderParam("num") int num){
 		return "La somma è " + (num+num);
 	}
+	
+	//Metodo per prendere i parametri dai cookies
+	@GET
+	@Path("/cookie")
+	public String getCookiesParameter(@CookieParam("num") int num){
+		return "La somma è " + (num+num);
+	}
+	
+	//Metodo per prendere i parametri dal context
+	@GET
+	@Path("/context")
+	public String getContextParameter(@Context UriInfo uriInfo, 
+			@Context HttpHeaders httpHeaders){
+//		String s= httpHeaders.getCookies().toString();
+		//Serve per hateos
+		String path= uriInfo.getAbsolutePath().toString();
+		return path;
+	}
+	
+	@GET
+	@Path("/fattura")
+	public String getContextParameter(@BeanParam FiltriFattura ff){
+		
+		return ff.getAnno()+ " " + ff.getOffset() + " " + ff.getLimit();
+	}
+	
+	
 }
